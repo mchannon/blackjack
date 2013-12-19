9ce1d7da314b06839d335891bb0e994fbc480b08
Matt Channon <mchannon@github.com>
2013-12-18 19:02:24 -0700

    #1: Initial Tabbed View Controller

---
+++ Blaxjack/Blaxjack/blaxjackSecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  blaxjackSecondViewController.h
+//  Blaxjack
+//
+//  Created by clamps on 12/18/13.
+//  Copyright (c) 2013 loitery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface blaxjackSecondViewController : UIViewController
+
+@end
---
+++ Blaxjack/Blaxjack/blaxjackSecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  blaxjackSecondViewController.m
+//  Blaxjack
+//
+//  Created by clamps on 12/18/13.
+//  Copyright (c) 2013 loitery. All rights reserved.
+//
+
+#import "blaxjackSecondViewController.h"
+
+@interface blaxjackSecondViewController ()
+
+@end
+
+@implementation blaxjackSecondViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
